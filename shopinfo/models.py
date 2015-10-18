ee8db56226479b13789e6ba198f8a6eea9423cc8
vancv <mimicat888@163.com>
2015-10-18 09:51:23 +0800

    return to normal.

--- shopinfo/models.py
+++ shopinfo/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Shopinformation(models.Model):
   id = models.AutoField(primary_key=True)
-  name = models.CharField(max_length=128,)
-  tel = models.CharField(max_length=64)
-  addr = models.CharField(max_length=256)
+  name = models.CharField(max_length=128, null=True, blank=True)
+  tel = models.CharField(max_length=64, null=True, blank=True)
+  addr = models.CharField(max_length=256, null=True, blank=True)
 
-  def __unicode__():
+  def __unicode__(self):
     return u'%s %s %s ;' %(self.name, self.tel, self.addr)
